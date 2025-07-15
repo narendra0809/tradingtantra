e1fc81c68e02aed476c4c675b42d5ce04c3729de
Narendra <np4375@gmail.com>
2025-07-15 07:04:04 +0530

    minor

--- Backend/src/config/optionChain.config.js
+++ Backend/src/config/optionChain.config.js
@@ -4,10 +4,10 @@ export default {
   accessToken: process.env.DHAN_ACCESS_TOKEN,
   clientId: process.env.DHAN_CLIENT_ID,
   underlyings: [
-    { name: 'NIFTY', scrip: 13, seg: 'IDX_I', stepSize: 75 },
-    { name: 'BANKNIFTY', scrip: 25, seg: 'IDX_I', stepSize: 30 },
-    { name: 'FINNIFTY', scrip: 27, seg: 'IDX_I', stepSize: 65 },
-    { name: 'MIDCPNIFTY', scrip: 442, seg: 'IDX_I', stepSize: 120 },
-    { name: 'SENSEX', scrip: 51, seg: 'IDX_I', stepSize: 20 },
+   { name: 'NIFTY', scrip: 13, seg: 'IDX_I', stepSize: 50 },
+    { name: 'BANKNIFTY', scrip: 25, seg: 'IDX_I', stepSize: 100 },
+    { name: 'FINNIFTY', scrip: 27, seg: 'IDX_I', stepSize: 50 },
+    { name: 'MIDCPNIFTY', scrip: 442, seg: 'IDX_I', stepSize: 75 },
+    { name: 'SENSEX', scrip: 51, seg: 'IDX_I', stepSize: 100 },
   ],
 };
