d2eafaaf0d24218254517d2732aff37d66ffb9ea
pratyush314 <pratyushshah314@gmail.com>
2025-07-22 20:46:46 +0530

    backend fix

--- Backend/src/utils/server-restart.js
+++ Backend/src/utils/server-restart.js
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { isProduction } from "../controllers/adminControllers/adminServer.controller";
 export const initiateRestart = (pm2Command) => {
   setTimeout(() => {
     exec(pm2Command, (error, stdout, stderr) => {
